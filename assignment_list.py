76af131b8268fa41502e1b6de20d9e81d292b6aa
Whodiduexpect <Whodiduexpect@users.noreply.github.com>
2019-09-04 21:40:46 -0700

    Fix function for Python 3.5
    You can't specify a path for os.path.exists in Python 3.5, and since it's an officialy supported Python version, I changed it to convert the path to a string

--- assignment_list.py
+++ assignment_list.py
@@ -31,7 +31,7 @@ def get_data_from_file(filename, split_char):
     return output
 
 def create_if_not_exist(filename):
-    if not os.path.exists(Path("data/%s" % filename)):
+    if not os.path.exists(str(Path("data/%s" % filename))):
         file = open_data(filename, "w")
         file.close()
 
