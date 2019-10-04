4bc1ee4c48568adba444697a9ce9a1db41ef54ce
Whodiduexpect <53096512+Whodiduexpect@users.noreply.github.com>
2019-10-03 20:46:29 -0700

    Fix test with no data folder

--- tests/help_screen_test.py
+++ tests/help_screen_test.py
@@ -1,11 +1,14 @@
 import pytest
 import subprocess
 import sys
+import os
 sys.path.append("")
 import assignment_list
 
 
 def test_answer():
+    if not os.path.exists("data"):
+        os.mkdir("data")
     assignment_list.createIfNotExist("studentvue_credentials")
     result = subprocess.run(['python3', 'assignment_list.py'], stdout=subprocess.PIPE)
     assert "Usage:" in result.stdout.decode("utf-8")
