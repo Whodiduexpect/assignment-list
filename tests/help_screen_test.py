df6eb3849dc355a68ae00d92b08d8f413a953277
Whodiduexpect <53096512+Whodiduexpect@users.noreply.github.com>
2019-10-03 20:43:11 -0700

    Update test to work with no data folder

--- tests/help_screen_test.py
+++ tests/help_screen_test.py
@@ -1,7 +1,11 @@
 import pytest
 import subprocess
+import sys
+sys.path.append("")
+import assignment_list
 
 
 def test_answer():
+    assignment_list.createIfNotExist("studentvue_credentials")
     result = subprocess.run(['python3', 'assignment_list.py'], stdout=subprocess.PIPE)
     assert "Usage:" in result.stdout.decode("utf-8")
