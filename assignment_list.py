7d000c01110ff9b574100fb5aaa0d60c177d6b48
Whodiduexpect <53096512+Whodiduexpect@users.noreply.github.com>
2019-10-04 23:51:46 -0700

    Add add feature

--- assignment_list.py
+++ assignment_list.py
@@ -5,24 +5,25 @@ import click
 import pandas as pd
 from pathlib import Path
 import studentvue_parser
+import random
 
 
 # Define functions
 def openData(file, mode):
-    return open(Path("data/%s" % file), mode)
+    return open(Path('data/%s' % file), mode)
 
 
 def listAssignments(assignments):
     if not len(assignments):
-        click.echo("No assignments to do!")
+        click.echo('No assignments to do!')
     else:
-        assignments_view = assignments[assignments["is_completed"] != True]
-        assignments_view = assignments_view.drop(columns=["is_completed"])
+        assignments_view = assignments[assignments['is_completed'] != True]
+        assignments_view = assignments_view.drop(columns=['is_completed'])
         click.echo(assignments_view)
 
 
 def getDataFromFile(filename, split_char):
-    file = openData(filename, "r")
+    file = openData(filename, 'r')
     output = file.read().split(split_char)
     file.close()
     return output
@@ -31,33 +32,33 @@ def getDataFromFile(filename, split_char):
 def updateCsv(assignments):
     assignments.to_csv(
         Path('data/studentvue_assignments.csv'),
-        sep="/",
+        sep='/',
         index=False)
 
 
 def createIfNotExist(filename):
-    if not os.path.exists(Path("data/%s" % filename)):
-        file = openData(filename, "w")
+    if not os.path.exists(Path('data/%s' % filename)):
+        file = openData(filename, 'w')
         file.close()
 
 
 def pointProblematicFile(path):
-    click.echo("Full path to problematic file: %s" % Path(path).absolute())
+    click.echo('Full path to problematic file: %s' % Path(path).absolute())
 
 
 # Main
 def main():
     # If the credential file does not exist:
-    if not os.path.exists(Path("data/studentvue_credentials")):
+    if not os.path.exists(Path('data/studentvue_credentials')):
         # If we don't even have a data folder:
-        if not os.path.exists("data"):
+        if not os.path.exists('data'):
             # Create one
-            os.mkdir("data")
+            os.mkdir('data')
         # Now that we know we have a data folder, let's set the credentials
         studentvue_parser.setCredentials()
 
     # Read the credential file
-    credentials = getDataFromFile("studentvue_credentials", ",")
+    credentials = getDataFromFile('studentvue_credentials', ',')
 
     # Define commands
     @click.group()
@@ -77,16 +78,16 @@ def main():
     @click.argument('category', required=False)
     def list(category):
         '''
-        List assignments by CATEGORY (defaults to "current")
+        List assignments by CATEGORY (defaults to 'current')
         '''
         if not category:
-            category = "current"
-        if category == "current":
+            category = 'current'
+        if category == 'current':
             assignments = studentvue_parser.getAssignments(credentials)
             listAssignments(assignments)
         else:
             click.echo(
-                "\"%s\" is not a valid category. The only valid category is current" %
+                '"%s" is not a valid category. Currently, the only valid category is current' %
                 category)
 
     @cli.command()
@@ -98,16 +99,62 @@ def main():
         assignments = studentvue_parser.getAssignments(credentials)
         try:
             assignments.at[assignments.loc[assignments['Assignment ID'].isin(
-                [id])].index, "is_completed"] = True
+                [id])].index, 'is_completed'] = True
             updateCsv(assignments)
         except BaseException:
-            click.echo("Failed to complete assignemnt #%s" % id)
+            click.echo('Failed to complete assignemnt #%s' % id)
             sys.exit()
-        click.echo("Marked assignment #%s as complete" % id)
+        click.echo('Marked assignment #%s as complete' % id)
 
+    @cli.command()
+    @click.argument('title')
+    @click.argument('date')
+    def add(title, date):
+        '''
+        Add a new assignment with the TITLE and DATE
+        '''
+        # Get schedule
+        classes = studentvue_parser.getSchedule(credentials)
+        # Print all the classes
+        for class_ in classes:
+            print(class_)
+        # Ask for which period
+        class_period_input = input(
+            '\nWhich class period do you want to add the assignment "%s" to? ' %
+            title)
+        class_period = int(class_period_input)
+
+        # Auto generate the class as if it was from Student Vue
+        teacher_fullname = classes[class_period - 1].teacher.name.split(' ')
+        teacher_label = "{0}, {1}".format(
+            teacher_fullname[1], teacher_fullname[0][:1])
+        class_name = "{0}  {1}({2})".format(teacher_label,
+                                            classes[class_period - 1].name,
+                                            classes[class_period - 1].period)
+
+        # Generate a assignment list 8 digit id, and make sure it's unique
+        assignments = studentvue_parser.getAssignments(credentials)
+        id_unique = False
+
+        while not id_unique:
+            generated_id = random.randrange(11111111, 99999999, 1)
+            is_id_unique = True
+            for assignment_id in assignments["Assignment ID"]:
+                if assignment_id == generated_id:
+                    is_id_unique = False
+            if is_id_unique:
+                id_unique = True
+
+        assignment_df = pd.DataFrame({'Assignment ID': [generated_id], 'Class Name': [
+                                     class_name], 'Due Date': [date], 'Assignment': [title], 'is_completed': ['False']})
+        assignments = assignments.append(assignment_df, sort=True)
+        convert_dict = {'Assignment ID': int}
+        assignments = assignments.astype(convert_dict)
+        updateCsv(assignments)
+        print(assignments)
     # Execute command
     cli(obj={})
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
--- studentvue_parser.py
+++ studentvue_parser.py
@@ -8,13 +8,13 @@ from getpass import getpass
 
 
 def setCredentials():
-    assignment_list.createIfNotExist("studentvue_credentials")
-    click.echo("-- Credentials setup/reset --")
-    student_id = input("Please enter Student ID: ")
+    assignment_list.createIfNotExist('studentvue_credentials')
+    click.echo('-- Credentials setup/reset --')
+    student_id = input('Please enter Student ID: ')
     password = getpass('Please enter password (hidden): ')
-    district_url = input("Please enter district login url: ")
-    file = assignment_list.openData("studentvue_credentials", "w")
-    file.write(student_id + "," + password + "," + district_url)
+    district_url = input('Please enter district login url: ')
+    file = assignment_list.openData('studentvue_credentials', 'w')
+    file.write(student_id + ',' + password + ',' + district_url)
     file.close()
     return True
 
@@ -25,31 +25,31 @@ def getAssignments(credentials):
         sv = StudentVue(credentials[0], credentials[1], credentials[2])
     except BaseException:
         click.echo(
-            "Invalid credentials. You might want to use:\n   python assignment_list.py reset")
+            'Invalid credentials. You might want to use:\n   python assignment_list.py reset')
         sys.exit()
 
     # Create CSV file if it dosen't exist
-    assignment_list.createIfNotExist("studentvue_assignments.csv")
+    assignment_list.createIfNotExist('studentvue_assignments.csv')
     assignments = []
     try:
         assignments = pd.read_csv(Path('data/studentvue_assignments.csv'),  # reads 'assignment_id', 'class_name', 'date', 'name', 'is_completed'
-                                  sep="/")
+                                  sep='/')
     except BaseException:
         assignments = pd.DataFrame({'Assignment ID': [], 'Class Name': [], 'Due Date': [
         ], 'Assignment': [], 'is_completed': []})
-        click.echo("Invalid or empty file detected... discarding file")
+        click.echo('Invalid or empty file detected... discarding file')
     try:
         studentvue_assignments = sv.get_assignments()
     except Exception as e:
         click.echo(
-            "Failed to get Student Vue assignments... Please see https://github.com/kajchang/StudentVue for help on this issue.\n The specific error is:",
+            'Failed to get Student Vue assignments... Please see https://github.com/kajchang/StudentVue for help on this issue.\n The specific error is:',
             sys.exc_info()[1])
         sys.exit()
     found_duplicate = False
     for sv_assignment in studentvue_assignments:
-        setattr(sv_assignment, 'date', str(sv_assignment.date.day) + "-" +
-                str(sv_assignment.date.month) + "-" + str(sv_assignment.date.year))
-        for assignment_id in assignments["Assignment ID"]:
+        setattr(sv_assignment, 'date', str(sv_assignment.date.day) + '/' +
+                str(sv_assignment.date.month) + '/' + str(sv_assignment.date.year))
+        for assignment_id in assignments['Assignment ID']:
             if sv_assignment.assignment_id == assignment_id:
                 found_duplicate = True
         if not found_duplicate:
@@ -59,7 +59,7 @@ def getAssignments(credentials):
                         sv_assignment.assignment_id], 'Class Name': [
                         sv_assignment.class_name], 'Due Date': [
                         sv_assignment.date], 'Assignment': [
-                        sv_assignment.name], 'is_completed': ["False"]})
+                        sv_assignment.name], 'is_completed': ['False']})
             assignments = assignments.append(assignment_df, sort=True)
     convert_dict = {'Assignment ID': int}
     assignments = assignments.astype(convert_dict)
@@ -67,5 +67,16 @@ def getAssignments(credentials):
     return assignments
 
 
-if __name__ == "__main__":
+def getSchedule(credentials):
+    # Login to Student Vue
+    try:
+        sv = StudentVue(credentials[0], credentials[1], credentials[2])
+    except BaseException:
+        click.echo(
+            'Invalid credentials. You might want to use:\n   python assignment_list.py reset')
+        sys.exit()
+    return sv.get_schedule()
+
+
+if __name__ == '__main__':
     click.echo("Don't run me! Run assignment_list.py instead.")
